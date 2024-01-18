12621732dcd38061034185d24feecc4de6ae183a
Rustam0220 <rustam20021015@gmail.com>
2024-01-19 00:11:46 +0600

    hw2bot

--- handlers/start.py
+++ handlers/start.py
@@ -2,28 +2,37 @@ import sqlite3
 
 from aiogram import types, Dispatcher
 from config import bot
+from config import bot, MEDIA_DESTINATION
 from database import db
+from const import START_MENU
 from keyboards import tg_buttons
 
 
 async def start_button(message: types.Message):
     datab = db.Database()
-    try:
-
-        datab.sql_insert_user(
-            tg_id=message.from_user.id,
-            username=message.from_user.username,
-            first_name=message.from_user.first_name,
-            last_name=message.from_user.last_name,
-        )
-    except sqlite3.IntegrityError:
-        pass
+    datab.sql_insert_user(
+        tg_id=message.from_user.id,
+        username=message.from_user.username,
+        first_name=message.from_user.first_name,
+        last_name=message.from_user.last_name,
+    )
     print(message)
     await bot.send_message(
         chat_id=message.from_user.id,
-        text=f'Hello {message.from_user.first_name}',
+        text=f"Hello {message.from_user.first_name}",
         reply_markup=await tg_buttons.start_keyboard()
     )
+    with open(MEDIA_DESTINATION + "bot-pic.jpeg", "rb") as photo:
+        await bot.send_photo(
+            chat_id=message.from_user.id,
+            photo=photo,
+            caption=START_MENU.format(
+                name=message.from_user.first_name
+            ),
+           reply_markup=await tg_buttons.start_keyboard()
+        )
+
+
 
 
 def register_start_handlers(dp: Dispatcher):
--- main.py
+++ main.py
@@ -3,22 +3,21 @@ from config import dp
 from handlers import (
     start,
     questionnaire,
+    chat_actions,
 )
 from database import db
-
 
 async def on_startup(_):
     datab = db.Database()
     datab.sql_create_tables()
 
-
 start.register_start_handlers(dp=dp)
 questionnaire.register_questionnaire_handlers(dp=dp)
+chat_actions.register_chat_actions_handlers(dp=dp)
 
 if __name__ == "__main__":
     executor.start_polling(
         dp,
-        skip_updates= True,
-        on_startup= on_startup
+        skip_updates=True,
+        on_startup=on_startup
     )
-
